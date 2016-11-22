571146c954cfd90431ce1ef0a0fb8a04a15259b1
Trosterud <sindre.reino.trosterud@gmail.com>
2016-11-22 13:42:31 +0200

    fixed misc

--- src/s1/sjakki/square.scala
+++ src/s1/sjakki/square.scala
@@ -2,30 +2,29 @@ package s1.sjakki
 
 class square {
  
-  private var occupant: Option[RobotBody] = None
+  private var occupant: Option[piece] = None
   
-  /** Returns `true` if the floor square has no robot in it, `false` otherwise. */
+  /** Returns `true` if the floor square has no piece in it, `false` otherwise. */
   def isEmpty = occupant == None
 
-  /** Returns the robot occupying the square, wrapped in an `Option`, or `None`, 
-  * if there is no robot in the square. */
-  def nappula: Option[RobotBody] = this.occupant
+  /** Returns the piece occupying the square, wrapped in an `Option`, or `None`, 
+  * if there is no piece in the square. */
+  def nappula: Option[piece] = this.occupant
 
-  /** Removes any robot from the square (if there was one there to begin with). */
+  /** Removes any piece from the square (if there was one there to begin with). */
   def clear(): Unit = {
     this.occupant = None
   }
 
-  /** Adds the given robot to the square, if possible. If there is something
-    * already in the square, a collision happens instead.
-    * @param arrivee  the robot arriving in the square
-    * @return `true` if `arrivee` was successfully placed in the square, `false` if a collision occurred */
- def addRobot(arrivee: RobotBody): Boolean = {
+  /** Adds the given piece to the square, if possible.
+    * @param arrivee  the piece arriving in the square
+    * @return `true` if `arrivee` was successfully placed in the square, else `false` */
+ def addpiece(arrivee: piece): Boolean = {
    if (this.isEmpty) {
      this.occupant = Some(arrivee)
      true
    } else {
-     this.occupant.foreach(_.destroy())
+//     this.occupant.foreach(_.destroy())
      false
    }
  }
