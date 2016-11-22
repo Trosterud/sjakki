16b23c76304be88ea2e93b5a876c288cfa5ab518
Trosterud <sindre.reino.trosterud@gmail.com>
2016-11-22 13:34:56 +0200

    fixed functions from robots

--- src/s1/sjakki/square.scala
+++ src/s1/sjakki/square.scala
@@ -1,32 +1,18 @@
 package s1.sjakki
 
 class square {
-  
-  
-  
-package o1.robots
-
-/** The trait `Square` represents squares in a robot world as an abstract concept.
-  *
-  * A square object is potentially mutable: its state can change as robots enter and exit it.
-  *
-  * Two concrete kinds of squares have been implemented: `Floor`s and `Wall`s.
-  *
-  * @see [[Floor]]
-  * @see [[Wall]] */
-Class Square {
-
+ 
   private var occupant: Option[RobotBody] = None
   
   /** Returns `true` if the floor square has no robot in it, `false` otherwise. */
   def isEmpty = occupant == None
 
-    /** Returns the robot occupying the square, wrapped in an `Option`, or `None`, 
-    * if there is no robot in the square. */
+  /** Returns the robot occupying the square, wrapped in an `Option`, or `None`, 
+  * if there is no robot in the square. */
   def nappula: Option[RobotBody] = this.occupant
 
   /** Removes any robot from the square (if there was one there to begin with). */
-  def clear() = {
+  def clear(): Unit = {
     this.occupant = None
   }
 
@@ -34,26 +20,7 @@ Class Square {
     * already in the square, a collision happens instead.
     * @param arrivee  the robot arriving in the square
     * @return `true` if `arrivee` was successfully placed in the square, `false` if a collision occurred */
-  def addRobot(arrivee: RobotBody): Boolean
-
-}
-
-
-
-
-
-  
-
-
-  
-
-  
-  /** Adds the given robot to the floor square, if possible. The arrival will fail,
-    * if the square already had an occupant. In that case, the occupant of the 
-    * square is destroyed (but the one that crashed into it stays intact).
-    * @param arrivee  the robot attempting to arrive in the square
-    * @return `true` if `arrivee` was successfully placed in the square, `false` if a collision occurred */
- def addRobot(arrivee: RobotBody) = {
+ def addRobot(arrivee: RobotBody): Boolean = {
    if (this.isEmpty) {
      this.occupant = Some(arrivee)
      true
@@ -62,10 +29,6 @@ Class Square {
      false
    }
  }
-
-}
-  
-  
-  
+}  
   
 }
