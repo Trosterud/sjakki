6f998427578a8ccf1b193119339641298f557ea2
Tuomas Pärssinen <Tuomas Prssinen>
2016-11-22 12:28:33 +0200

    fleshed out bishop class

--- src/s1/sjakki/bishop.scala
+++ src/s1/sjakki/bishop.scala
@@ -1,6 +1,24 @@
-package s1.sjakki
+package sjakki
+
+import o1.grid._
+
+class bishop (name: String, attemptMove: Coords)  {
+
+    var currentLocation = this.location
+    var team = whites //or blacks
+
+    def move() = {
+      if (ONKO SIIRTO SÄÄNTÖJEN MUKAINEN) {
+        if (this.world.elementAt(attemptMove).NAPPULA.team == this.team) {//jos halutussa ruudussa on oma nappula (NAPPULA = robotbody)
+            println("Can't move. The square is already occupied by your own team member")
+          } else if (this.world.elementAt(attemptMove).NAPPULA.team != this.team) {  //jos halutussa ruudussa on vihollis nappula (NAPPULA = robotbody)
+            SYÖDÄÄN VASTUSTAJA
+            this.location = attemptMove //LIIKUTAAN RUUTUUN
+          } else if (!this.world.elementAt(attemptMove).NAPPULA.isDefined) { //ruutu on tyhjä(huomaa huutomerkki)
+            this.location = attemptMove //LIIKUTAAN RUUTUUN
+          } else println("Can't move!")
+        }
+}
 
 
-class bishop {
-  
 }
