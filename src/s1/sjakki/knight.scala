e1779f6fea35918d5ef3817ff2550ee48e113842
Paerssinen <tuomas.parssinen@hotmail.com>
2016-12-05 15:07:59 +0200

    virheet poistettu eclipsen kanssa

--- src/s1/sjakki/knight.scala
+++ src/s1/sjakki/knight.scala
@@ -1,63 +1,60 @@
-package s1.sjakki
+package o1.robots
 
 import o1.grid._
 
-class Knight (name: String, attemptMove: Coords, team: Team) extends Nappula(team) {
+//Nappula = RobotBody
+//Team = RobotBrain
 
+class Knight(name: String, attemptMove: Coords, team: Team, nappula: Nappula) extends Team(name, nappula) {
 
   //Bishopista
-    var checkSquare = this.location//tästä lähdetään liikkeelle ja tätä liikutetaan diagonaalisti jokaiseen ilmansuuntaan vuorotellen
-    var compassIndex = 0 //käy läpi 0,1,2,3 eli kaikki diagonaalit
-    var isOkToMove = false //tarkastuksen tulos, oletuksena FALSE
-    var firstEnemy = false //pitää kirjaa onko kohdattu vihollinen ensimmäinen, eli estääe ettei vihun yli "hypätä" sallittuja liikkeitä laskiessa.
+  var checkSquare = this.location //tästä lähdetään liikkeelle ja tätä liikutetaan diagonaalisti jokaiseen ilmansuuntaan vuorotellen
+  var compassIndex = 0 //käy läpi 0,1,2,3 eli kaikki diagonaalit
+  var isOkToMove = false //tarkastuksen tulos, oletuksena FALSE
+  var firstEnemy = false //pitää kirjaa onko kohdattu vihollinen ensimmäinen, eli estääe ettei vihun yli "hypätä" sallittuja liikkeitä laskiessa.
 
   //Towerista
-    var compass = Direction.Clockwise//Vectori jossa ilmansuunnat
+  var compass = Direction.Clockwise //Vectori jossa ilmansuunnat
 
-
-//Liikutetaan checkSquare:a siten, että tarkastetaan onko attemptMove checkSquare ja jos on niin palautetaan lopuksi true.
-    def canMove(attemptMove): Boolean = { //ONKO SIIRTO SÄÄNTÖJEN MUKAINEN, jos on niin palauttaa TRUE, jos ei niin FALSE
-      //Towerin ilmansuunnille
-      while (compassIndex <= 3 && isOkToMove == false) {
-          ilmansuunta = compass(compassIndex)
-          checkSquare = checkSquare.neighbor(ilmansuunta)//liikutetaan tarkastusruutua
-          checkSquare = checkSquare.neighbor(ilmansuunta)//liikutetaan tarkastusruutua
-          //tarkastetaan oikean puoleiset ruudut
-          oikealle = (compassIndex + 1) % compass.length
-          checkSquare = checkSquare.neighbor(compass(oikealle))
-            if (this.world.elementAt(checkSquare) == attemptMove) {
-            isOkToMove = TRUE //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
-            }
-          //tarkastetaan vasemman puoleiset ruudut
-          vasemmalle = (compassIndex + 3) % compass.length
-          checkSquare = checkSquare.neighbor(compass(vasemmalle))
-            if (this.world.elementAt(checkSquare) == attemptMove) {
-            isOkToMove = TRUE //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
-            }
-          //aloitetaan uusi ilmansuunta
-            checkSquare = this.location
-            compassIndex = compassIndex + 1
+  //Liikutetaan checkSquare:a siten, että tarkastetaan onko attemptMove checkSquare ja jos on niin palautetaan lopuksi true.
+  def canMove(attemptMove: Coords): Boolean = { //ONKO SIIRTO SÄÄNTÖJEN MUKAINEN, jos on niin palauttaa TRUE, jos ei niin FALSE
+    //Towerin ilmansuunnille
+    while (compassIndex <= 3 && isOkToMove == false) {
+      var ilmansuunta = compass(compassIndex)
+      checkSquare = checkSquare.neighbor(ilmansuunta) //liikutetaan tarkastusruutua
+      checkSquare = checkSquare.neighbor(ilmansuunta) //liikutetaan tarkastusruutua
+      //tarkastetaan oikean puoleiset ruudut
+      var oikealle = (compassIndex + 1) % compass.length
+      checkSquare = checkSquare.neighbor(compass(oikealle))
+      if (this.world.elementAt(checkSquare) == attemptMove) {
+        isOkToMove = true //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
       }
-      isOkToMove //Palautetaan etsinnän tulos
+      //tarkastetaan vasemman puoleiset ruudut
+      var vasemmalle = (compassIndex + 3) % compass.length
+      checkSquare = checkSquare.neighbor(compass(vasemmalle))
+      if (this.world.elementAt(checkSquare) == attemptMove) {
+        isOkToMove = true //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
+      }
+      //aloitetaan uusi ilmansuunta
+      checkSquare = this.location
+      compassIndex = compassIndex + 1
     }
-
-
-
-    def move() = {
-      if (canMove(attemptMove)) {
-            if (this.world.elementAt(attemptMove).NAPPULA.team == this.team) {//jos halutussa ruudussa on oma nappula (NAPPULA = robotbody)
-            println("Can't move. The square is already occupied by your own team member")
-          } else if (this.world.elementAt(attemptMove).NAPPULA.team != this.team) {  //jos halutussa ruudussa on vihollis nappula (NAPPULA = robotbody)
-            this.world.elementAt(attemptMove).NAPPULA.remove//SYÖDÄÄN VASTUSTAJA
-            this.location = attemptMove //LIIKUTAAN RUUTUUN
-          } else if (this.world.elementAt(attemptMove).NAPPULA.isEmpty) { //ruutu on tyhjä
-            this.location = attemptMove //LIIKUTAAN RUUTUUN
-          } else println("Can't move!")
-      } else ("Rule violation, can't move!") //siirto ei ole sääntöjen mukainen
+    isOkToMove //Palautetaan etsinnän tulos
   }
 
-
-
-
+  def move() = {
+    if (canMove(attemptMove)) {
+      if (this.world.elementAt(attemptMove).nappula.get.team == this.team) { //jos halutussa ruudussa on oma nappula (NAPPULA = robotnappula)
+        println("Can't move. The square is already occupied by your own team member")
+      } else if (this.world.elementAt(attemptMove).nappula.get.team != this.team && this.world.elementAt(attemptMove).isEmpty) { //jos halutussa ruudussa on vihollis nappula (NAPPULA = robotnappula)
+        this.world.elementAt(attemptMove).nappula.get.locationSquare.clear() //SYÖDÄÄN VASTUSTAJA, tämä ei taida toimia
+        this.nappula.locationSquare.clear() //tyhjennetään lähtöruutu
+        new Knight(name: String, attemptMove: Coords, team: Team, nappula: Nappula) //LIIKUTAAN RUUTUUN, eli ruutuun luodaan uusi nappula
+      } else if (this.world.elementAt(attemptMove).isEmpty) { //ruutu on tyhjä
+        this.nappula.locationSquare.clear() //tyhjennetään lähtöruutu
+        new Knight(name: String, attemptMove: Coords, team: Team, nappula: Nappula) //LIIKUTAAN RUUTUUN
+      } else println("Can't move!")
+    } else ("Rule violation, can't move!") //siirto ei ole sääntöjen mukainen
+  }
 
 }
