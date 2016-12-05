ab3ac3e864d86aff69d5c9a23f5a0a8edc6c8162
Paerssinen <tuomas.parssinen@hotmail.com>
2016-12-05 15:09:42 +0200

    virheet poistettu eclipsen kanssa

--- src/s1/sjakki/pawn.scala
+++ src/s1/sjakki/pawn.scala
@@ -1,9 +1,11 @@
-package s1.sjakki
+package o1.robots
 
 import o1.grid._
 
-class Pawn (name: String, attemptMove: Coords, team: Team) extends Nappula(team) {
+//Nappula = RobotBody
+//Team = RobotBrain
 
+class Pawn(name: String, attemptMove: Coords, team: Team, nappula: Nappula) extends Team(name, nappula) {
 
   var currentLocation = this.location
 
@@ -18,33 +20,33 @@ class Pawn (name: String, attemptMove: Coords, team: Team) extends Nappula(team)
 
 
 //tarkastaa saako PAWN liikkua attemptMove:n suuntaan. Palauttaa true, jos checkSquare osuu attemptSquareen, eli attemptMove löytyy "sallitulta" alueelta.
-  def canMove(attemptMove): Boolean = { //ONKO SIIRTO SÄÄNTÖJEN MUKAINEN, jos on niin palauttaa TRUE, jos ei niin FALSE
+  def canMove(attemptMove: Coords): Boolean = { //ONKO SIIRTO SÄÄNTÖJEN MUKAINEN, jos on niin palauttaa TRUE, jos ei niin FALSE
   //Käy läpi ensimmäisen siirron
   if (isFirstMove == true) {
-  upwards = compass(0)
+  var upwards = compass(0)
   checkSquare = checkSquare.neighbor(upwards)
-    if (this.world.elementAt(checkSquare) == attemptMove || this.world.elementAt(checkSquare.neighbor(upwards))) { //katsoo kaksi ensimmäistä ruutua
-      isOkToMove = TRUE //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
+    if (this.world.elementAt(checkSquare) == attemptMove || this.world.elementAt(checkSquare.neighbor(upwards)) == attemptMove) { //katsoo kaksi ensimmäistä ruutua
+      isOkToMove = true //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
       isFirstMove = false
     }
 } else {
   //Käy läpi koillisen ja luoteen
   checkSquare = this.location
   while (compassIndex <= 3 && isOkToMove == false) { //tämä loopataan kahdesti, koska kaksi ilmansuuntaa
-        diagonal = diagonalCompass(compassIndex)
+        var diagonal = diagonalCompass(compassIndex)
         checkSquare = checkSquare.neighbor(diagonal) //Tämä pätkä liikuttaa checkSquarea diagonaalisti yhden kerran
-              if (this.world.elementAt(checkSquare) == attemptMove && this.world.elementAt(checkSquare).NAPPULA.team != this.team ) {
-              isOkToMove = TRUE //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
+              if (this.world.elementAt(checkSquare) == attemptMove && this.world.elementAt(attemptMove).nappula.get.team != this.team) {
+              isOkToMove = true //attemptMove löytyi sallitulta alueelta. Palautetaan TRUE ja lopetetaan etsintä
               }
         checkSquare = this.location
         compassIndex = compassIndex + 3 //tarkastetaan vain koillinen ja luode
   }
   //tarkastaa liikkumisen suoraan ylöspäin
   if (isOkToMove == false) {
-    upwards = compass(0)//pohjoinen
+    var upwards = compass(0)//pohjoinen
     checkSquare = checkSquare.neighbor(upwards)
       if (this.world.elementAt(checkSquare) == attemptMove) {
-        isOkToMove = TRUE
+        isOkToMove = true
       }
   }
   }
@@ -52,22 +54,19 @@ class Pawn (name: String, attemptMove: Coords, team: Team) extends Nappula(team)
   isOkToMove
   }
 
-
-  //"päämetodi" Tämä kutsuu canMove metodia ja liikuttaa nappulaa sinne, jos siirto on sääntöjen mukainen. Syö myös vastustajan nappulan tarvittaessa.
-      def move() = {
-        if (canMove(attemptMove)) {
-              if (this.world.elementAt(attemptMove).NAPPULA.team == this.team) {//jos halutussa ruudussa on oma nappula (NAPPULA = robotbody)
-              println("Can't move. The square is already occupied by your own team member")
-            } else if (this.world.elementAt(attemptMove).NAPPULA.team != this.team && this.world.elementAt(checkSquare.neighbor(diagonalCompass(0))) || this.world.elementAt(checkSquare.neighbor(diagonalCompass(3))) ) {  //vihollinen ja viistossa oikealle tai vasemmalle)
-              this.world.elementAt(attemptMove).NAPPULA.remove//SYÖDÄÄN VASTUSTAJA
-              this.location = attemptMove //LIIKUTAAN RUUTUUN
-            } else if (this.world.elementAt(attemptMove).NAPPULA.isEmpty && this.world.elementAt(checkSquare.neighbor(compass(0)))) { //ruutu on tyhjä ja se on ylöspäin
-              this.location = attemptMove //LIIKUTAAN RUUTUUN
-            } else println("Can't move!")
-        } else ("Rule violation, can't move!") //siirto ei ole sääntöjen mukainen
+  def move() = {
+    if (canMove(attemptMove)) {
+      if (this.world.elementAt(attemptMove).nappula.get.team == this.team) { //jos halutussa ruudussa on oma nappula (NAPPULA = robotnappula)
+        println("Can't move. The square is already occupied by your own team member")
+      } else if (this.world.elementAt(attemptMove).nappula.get.team != this.team && this.world.elementAt(attemptMove).isEmpty) { //jos halutussa ruudussa on vihollis nappula (NAPPULA = robotnappula)
+        this.world.elementAt(attemptMove).nappula.get.locationSquare.clear() //SYÖDÄÄN VASTUSTAJA, tämä ei taida toimia
+        this.nappula.locationSquare.clear() //tyhjennetään lähtöruutu
+        new Pawn(name: String, attemptMove: Coords, team: Team, nappula: Nappula) //LIIKUTAAN RUUTUUN, eli ruutuun luodaan uusi nappula
+      } else if (this.world.elementAt(attemptMove).isEmpty) { //ruutu on tyhjä
+        this.nappula.locationSquare.clear() //tyhjennetään lähtöruutu
+        new Pawn(name: String, attemptMove: Coords, team: Team, nappula: Nappula) //LIIKUTAAN RUUTUUN
+      } else println("Can't move!")
+    } else ("Rule violation, can't move!") //siirto ei ole sääntöjen mukainen
   }
-
-
-
 
 }
